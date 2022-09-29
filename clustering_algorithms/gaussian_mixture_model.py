8e467bfed36dc86c8c5b9cdf9fc5631c3908e79a
BINDILA <MBINDILA@steelcase.com>
2022-09-29 15:24:20 +0300

    created GaussianMixtureModel class

---
+++ clustering_algorithms/gaussian_mixture_model.py
@@ -0,0 +1,11 @@
+from clustering_algorithm import ClusteringAlgorithm
+
+class GaussianMixtureModel(ClusteringAlgorithm):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit_transform(self):
+        pass
+
+    def score(self, scorer):
+        return super().score(scorer)
